e36bdf7ab24a49adced2318179b436e602b8c05e
muratcan-yuksel <muratcanyukselpro@gmail.com>
2021-09-10 03:10:17 +0300

    made it responsive

--- src/App.js
+++ src/App.js
@@ -3,22 +3,37 @@ import SecondPart from "./components/SecondPart";
 import FirstPart from "./components/firstPart";
 import ThirdPart from "./components/thirdPart";
 import LastPart from "./components/lastPart";
+import { useMediaQuery } from "react-responsive";
 
 function App() {
+  const isTabletOrMobile = useMediaQuery({ query: "(max-width: 1224px)" });
+  const isDesktopOrLaptop = useMediaQuery({
+    query: "(min-width: 1224px)",
+  });
   return (
     <div className="App">
-      <div className="splitScreen">
-        <div className="topPane">
+      {isDesktopOrLaptop && (
+        <div className="splitScreen">
+          <div className="topPane">
+            <FirstPart />
+          </div>
+          <div className="secondPane">
+            <SecondPart />
+          </div>
+          <div className="middlePane">
+            <LastPart />
+            <ThirdPart />
+          </div>
+        </div>
+      )}
+      {isTabletOrMobile && (
+        <div style={({ display: "flex" }, { flexDirection: "column" })}>
           <FirstPart />
-        </div>
-        <div className="secondPane">
-          <SecondPart />
-        </div>
-        <div className="middlePane">
           <LastPart />
+          <SecondPart />
           <ThirdPart />
         </div>
-      </div>
+      )}
     </div>
   );
 }
